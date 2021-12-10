1c5f5706009abd7762c70616791bdc6a0493c204
dumTO <apbtheod@gmail.com>
2021-12-10 21:07:41 +0100

    commit premier push in dataset

--- src/main/java/semweb/App.java
+++ src/main/java/semweb/App.java
@@ -1,5 +1,7 @@
 package semweb;
 
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 //import org.apache.jena.rdf.model.Property;
@@ -12,6 +14,7 @@ import org.apache.jena.rdfconnection.RDFConnectionFactory;
 //import org.apache.jena.riot.Lang;
 //import org.apache.jena.riot.RDFDataMgr;
 //import org.apache.jena.vocabulary.VCARD;
+import org.apache.jena.tdb.TDBFactory;
 
 /**
  * Hello world!
@@ -23,18 +26,26 @@ public class App
     {
         //Model model = ModelFactory.createDefaultModel();
         // ... build the model
-        final String url = "https://territoire.emse.fr/kg/ontology.nt";
-        final Model model = ModelFactory.createDefaultModel();
+        /*
+        String url = "https://territoire.emse.fr/kg/ontology.nt";
+        RDFConnection conneg = RDFConnectionFactory.connect(url);
+        Model model = conneg.fetch("http://localhost:3030/ds");
         model.read(url);
-        model.write(System.out);
-        
-        String datasetURL = "http://localhost:3030/dataset.html";
+        conneg.load(model);
+        conneg.put(model);
+
+*/
+        String url = "https://territoire.emse.fr/kg/ontology.nt";
+        Model model = ModelFactory.createDefaultModel();
+        model.read(url);
+// ... build the model
+        String datasetURL = "http://localhost:3030/ds";
         String sparqlEndpoint = datasetURL + "/sparql";
         String sparqlUpdate = datasetURL + "/update";
         String graphStore = datasetURL + "/data";
         RDFConnection conneg = RDFConnectionFactory.connect(sparqlEndpoint,sparqlUpdate,graphStore);
         conneg.load(model); // add the content of model to the triplestore
-        //conneg.update("INSERT DATA { <test> a <TestClass> }"); // add the triple to the triplestore
+        conneg.update("INSERT DATA { <test> a <TestClass> }"); // add the triple to the triplestore
         
 
         System.out.println( "Hello World!" );
